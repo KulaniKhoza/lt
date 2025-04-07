b2d918ad93eab7fd6016bb274bc5deec6d359bf6
Kulani Khoza <kulanikhoza16@gmail.com>
2025-04-07 18:36:23 +0200

    test_getbest.py unit test (array method??)

--- test_getbest.py
+++ test_getbest.py
@@ -1,19 +1,19 @@
-from getbest import *
+import getbest
 import unittest
+from io import StringIO
+class TestingGetBest(unittest.Testcase):
 
-class TestingBest(unittest.Testcase):
-def setup(self)
-name = "Student Number"
-markCol = "Mark"
-
+def setUp(self):
+self.data = [
+            ["ELEN3020", "StudentNumber", "Mark", "Comment"],
+            ["ELEN3020", "160001", "72", "OK"],
+            ["ELEN3020", "167381", "90", "Check"],
+            ["ELEN3020", "143211", "83", "-"]
+        ]
 def test_getCols(self):
-num_col = 1 
-i = 1
-mark_col = 2
-self.assertEqual(num_col,i)
-selfassertEqual(mark_col,i+1)
-
-def test_findTop(self)
+num_col, mark_col = getbest.getCols(self.data[0])
+self.assertEqual(num_col, 0)
+self.assertEqual(mark_col, 2))
 
 if __name__ == '__main__':
 unittest.main()
